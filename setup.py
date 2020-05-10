adea6adced8dfaa1fde0119cc36363aca2b17dea
David Brochart <david.brochart@gmail.com>
2020-05-10 14:55:34 +0200

    v0.1.1

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/davidbrochart/xarray_leaflet',
-    version='0.1.0',
+    version='0.1.1',
     zip_safe=False,
     data_files=get_data_files()
 )
